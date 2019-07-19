6c7568b5727f91d9b959baa0140d8338cef2a0ed
Joel Klinger <joel.alexander.klinger@gmail.com>
2019-07-19 16:33:11 +0100

    Added deployment

--- clio_lite.py
+++ clio_lite.py
@@ -1,27 +1,35 @@
-# ENDPOINT IS AN ENVIRONMENTAL PARAMETER
-# minimum_should_match is parameters
-
 import json
 from botocore.vendored import requests
-
-ENDPOINT = ('https://search-health-scanner-'
-            '5cs7g52446h7qscocqmiky5dn4.eu-west-2.es.amazonaws.com')
+import os
 
 
-def try_pop(x, k):
+def try_pop(x, k, default=None):
     try:
         v = x.pop(k)
     except KeyError:
-        v = None
+        v = default
     finally:
         return v
 
 
 def lambda_handler(event, context=None):
-    url = f"{ENDPOINT}/{event['pathParameters']['proxy']}"
     query = json.loads(event['body'])
+
+    # Extract info from the query as required
     _from = try_pop(query, 'from')
     _size = try_pop(query, 'size')
+    min_term_freq = try_pop(query, 'min_term_freq', 2)
+    max_query_terms = try_pop(query, 'max_query_terms', 25)
+    min_doc_freq = try_pop(query, 'min_doc_freq', 5)
+    max_doc_frac = try_pop(query, 'max_doc_frac', 0.95)
+    minimum_should_match = try_pop(query, 'minimum_should_match',
+                                   '30%')
+
+    # Generate the endpoint URL, and validate
+    endpoint = query.pop('es_endpoint')
+    if endpoint not in os.environ['ALLOWED_ENDPOINTS'].split(","):
+        raise ValueError(f'{endpoint} has not been registered')
+    url = f"https://{endpoint}/{event['pathParameters']['proxy']}"
 
     # Make the initial request
     r = requests.post(url, data=json.dumps(query),
@@ -31,23 +39,23 @@ def lambda_handler(event, context=None):
     # Formulate the MLT query
     docs = [{'_id': row['_id'], '_index': row['_index']}
             for row in data['hits']['hits']]
-    max_doc_freq = int(0.95*data['hits']['total'])
+    max_doc_freq = int(max_doc_frac*data['hits']['total'])
     mlt_query = {"query":
                  {"more_like_this":
                   {"fields": ["body"],
                    "like": docs,
-                   "min_term_freq": 2,
-                   "max_query_terms": 25,
-                   "min_doc_freq": 5,
+                   "min_term_freq": min_term_freq,
+                   "max_query_terms": max_query_terms,
+                   "min_doc_freq": min_doc_freq,
                    "max_doc_freq": max_doc_freq,
                    "boost_terms": 1.,
-                   "minimum_should_match": "50%",
+                   "minimum_should_match": minimum_should_match,
                    "include": True}}}
     if _from is not None:
         mlt_query['from'] = _from
     if _size is not None:
         mlt_query['size'] = _size
-    
+
     # Make the new query and return
     r = requests.post(url, data=json.dumps(mlt_query),
                       headers=event['headers'])
---
+++ deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+FUNCTION_NAME=$(cat config/function_name)
+ALLOWED_ENDPOINTS=$(cat config/allowed_endpoints)
+
+set -e
+set -x
+
+# Run tests
+pytest
+
+# Get latest code version
+VERSION=$(git for-each-ref refs/tags\
+	      --sort=-taggerdate \
+	      --format='%(refname)' \
+	      --count=1 | cut -c 12-)
+
+# Check if lambda exists
+set +e
+set +x
+aws lambda get-function --function-name $FUNCTION_NAME:Version$VERSION &> /dev/null
+
+if [ $? -ne 0 ]
+then
+    echo "Version $VERSION not found"
+    # Deploy new lambda version
+    zip clio_lite.zip clio_lite.py
+    PYCODE_="import sys, json; print(json.load(sys.stdin)['Version'])"
+    FUNCTION_VERSION=$(aws lambda update-function-code \
+			   --function-name $FUNCTION_NAME \
+			   --zip-file fileb://clio_lite.zip \
+			   --publish | python -c "$PYCODE_")
+    rm clio_lite.zip
+    
+    # Set the new alias
+    echo "Created lambda function with pseudoversion $FUNCTION_VERSION"
+    aws lambda create-alias \
+	--function-name $FUNCTION_NAME \
+	--name Version$VERSION \
+	--function-version=$FUNCTION_VERSION
+
+    # Pick up the allowed endpoints
+    aws lambda update-function-configuration \
+	--function-name $FUNCTION_NAME \
+	--environment Variables={ALLOWED_ENDPOINTS=$ALLOWED_ENDPOINTS}
+else
+    echo "Nothing to do for version $VERSION"
+fi
